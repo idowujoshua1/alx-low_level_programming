d49a337a91ae7f1cc1bf9aa6fd5920a09d4fc457
idowujoshua1 <jonlaney346@gmail.com>
2023-10-05 15:11:46 -0700

    tough one here

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints strings that is followd by a new line
- * Return - Program returns to zero
- int main()
+*main - prints strings that is followd by a new line
+*Return: 0 (success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
