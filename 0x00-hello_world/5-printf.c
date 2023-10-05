cefadf9663d7089fe70cd252976de3e8dd6c74aa
idowujoshua1 <jonlaney346@gmail.com>
2023-10-05 14:16:00 -0700

    a C program that prints a sting followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints strings that is followd by a new line
+ * Return - Program returns to zero
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
