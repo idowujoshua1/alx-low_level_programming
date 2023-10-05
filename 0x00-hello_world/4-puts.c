f437400e7faa52d442f79ce20c8fb8fd2bcae8f9
idowujoshua1 <jonlaney346@gmail.com>
2023-10-05 12:04:19 -0700

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a string, followed by a new line.
+ * Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
