5b8067a8d85661450e4fd7a4b017092eba2857f6
idowujoshua1 <jonlaney346@gmail.com>
2023-10-05 18:29:04 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints eactly and that piece of art is useful
+ * main - prints eactly "and that piece of art is useful"
  * Return: 1(Success)
  */
 int main(void)
 {
-write(2, and that piece of art is useful"\ - Dora Korpar, 2015-10-19\n"66);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 69);
 return (1);
 }
