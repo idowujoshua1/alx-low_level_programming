bb878143c1ef830b2ce62f3a28ef4107ecbbd6c5
idowujoshua1 <jonlaney346@gmail.com>
2023-10-06 19:10:37 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Enty point
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
