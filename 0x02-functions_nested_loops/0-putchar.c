f9ac66bbc5311b9fc0eeda9447e85fe705aa1710
idowujoshua1 <jonlaney346@gmail.com>
2023-10-10 10:21:44 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 /**
- * main - prints _putchar
+ * main - Custom _putchar function
+ * This function takes a character as input and prints it to standard output.
  * Return: 0 Always
  */
 int main(void)
 {
-	printf("_putchar\n");
+	/**
+	 * Call the custom putchar function
+	 */
+	_putchar("_\n");
+	_putchar("p\n");
+	_putchar("u\n");
+	_putchar("t\n");
+	_putchar("c\n");
+	_putchar("h\n");
+	_putchar("a\n");
+	_putchar("r\n");
 	return (0);
 }
