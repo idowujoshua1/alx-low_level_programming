491915ea3c383e50fa22e5777246cacc5464e27e
idowujoshua1 <jonlaney346@gmail.com>
2023-10-11 09:09:38 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower - check if lower or upper
+*
+* @c: Parameter
+*
+* Return: 1 (True) 0 (False)
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
