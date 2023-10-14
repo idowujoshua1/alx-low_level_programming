5c432e1f17fb4b3f9c16efd2680c9b5a65fbfe8b
idowujoshua1 <jonlaney346@gmail.com>
2023-10-14 15:56:52 -0700

    distinguishing prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,9 +1,9 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main -  prints the largest prime factor of the number 612852475143
  * Return: Always 0.
- */
+ **/
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
