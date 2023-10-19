187dbbf5df546deb652b5c89c6638d04468f2313
idowujoshua1 <jonlaney346@gmail.com>
2023-10-19 08:45:48 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MIAN_H
+
+/**
+ * This header file has all the prototypes for the 0x06. C - More pointers, arrays and dtrings
+ */
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+#endif
