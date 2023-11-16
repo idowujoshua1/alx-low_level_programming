0394fa9d33df9b44734747f00bde148e4e048c79
idowujoshua1 <jonlaney346@gmail.com>
2023-11-16 15:19:46 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: singly linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+size_t node;
+node = 0;
+while (h != NULL)
+{
+h = h->next;
+node++;
+}
+return (node);
+}
