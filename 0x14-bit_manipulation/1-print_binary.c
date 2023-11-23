d65b0c2c8e1ccfd0062eb229a227b060ac8f991e
idowujoshua1 <jonlaney346@gmail.com>
2023-11-23 13:32:07 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_binary -  prints the binary representation of a number.
- * @n: the number represented
- * Return: 0
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	int shift = sizeof(unsigned long int) * 8 - 1;
-
-	while (shift >= 0)
-	{
-		putchar((n & (1UL << shift)) ? '1' : '0');
-		shift--;
-	}
-
-	printf("\n");
+	if (n > 1)
+	print_binary(n >> 1);
+	putchar((n & 1) + '0');
 }
