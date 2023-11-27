c187b987ecf6b66f2c317466e20b1ffb513dbc2a
idowujoshua1 <jonlaney346@gmail.com>
2023-11-27 14:26:30 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* create_file - Function that creates a file.
+* @filename: file to be read.
+* @text_content: NULL terminated string to write to the file.
+* Return: 1 on success and -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, len = 0;
+
+if (filename == NULL)
+	return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+	return (-1);
+
+if (text_content != NULL)
+{
+	while (text_content[len])
+	len++;
+	if (write(fd, text_content, len) != len)
+	{
+	close(fd);
+	return (-1);
+	}
+}
+
+close(fd);
+return (1);
+}
